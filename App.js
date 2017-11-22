8fe0f23e73c9ddefa63540beab6179cf8770b710
Omri Hopson <omri@Omris-MacBook-Pro.local>
2017-11-21 23:37:07 -0500

    added animation

--- App.js
+++ App.js
@@ -11,24 +11,58 @@ import {
   Text,
   Image,
   ImageBackground,
-  View
+  Animated,
+  View,
+  ScrollView
 } from 'react-native';
 
 import BackgroundImage from './components/BackgroundImage';
 
 
+class FadeInView extends React.Component {
+  state = {
+    fadeAnim: new Animated.Value(0),  // Initial value for opacity: 0
+  }
+
+  componentDidMount() {
+    Animated.timing(                  // Animate over time
+      this.state.fadeAnim,            // The animated value to drive
+      {
+        toValue: 1,                   // Animate to opacity: 1 (opaque)
+        duration: 2500,              // Make it take a while
+      }
+    ).start();                        // Starts the animation
+  }
+
+  render() {
+    let { fadeAnim } = this.state;
+
+    return (
+      <Animated.View                 // Special animatable View
+        style={{
+          ...this.props.style,
+          opacity: fadeAnim,         // Bind opacity to animated value
+        }}
+      >
+        {this.props.children}
+      </Animated.View>
+    );
+  }
+}
+
 export default class App extends Component {
   render() {
     return (
-      <View style = {styles.container}>
+      <View style={styles.container}>
         <BackgroundImage
           source={require('./images/jamiaca_beach.png')}
           originalWidth = {1440}
           originalHeight = {2560}
-        />
+        ></BackgroundImage>
+        <FadeInView style = {{height:250 , width:250,position:'absolute'}}>
+          <Animated.Image style = {{position:'absolute', height:250 , width:250,resizeMode:'contain'}} source={require('./images/NCB-Financial-Group.png')} ></Animated.Image>
+        </FadeInView>
       </View>
-
-
     );
   }
 }
@@ -42,7 +76,7 @@ const styles = StyleSheet.create({
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    color: 'white',
   },
 
 });
--- components/BackgroundImage.js
+++ components/BackgroundImage.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-import {Image, Dimensions, StyleSheet} from 'react-native';
+import {Image, Dimensions, StyleSheet, View} from 'react-native';
 
 const BackgroundImage = ({source, originalWidth, originalHeight}) => {
     let windowWidth = Dimensions.get('window').width
@@ -9,19 +9,18 @@ const BackgroundImage = ({source, originalWidth, originalHeight}) => {
     let newHeight = Dimensions.get('window').height
 
     return (
-        <Image source = {source} style = {{width: newWidth, height: newHeight, resizeMode: 'stretch', position: 'relative'}}
-        />
+        <Image source = {source} style = {{width: newWidth, height: newHeight, resizeMode: 'stretch', position: 'relative'}}></Image>
 
     )
 }
 
 
 const styles = StyleSheet.create({
-    background: {
-      // resizeMode: 'conver',
-      // position: 'relative',
-      // alignItems: 'center',
-    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        color: '#ffffff',
+      },
   });
 
 export default BackgroundImage;
